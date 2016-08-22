afaa0fe603b94d09d639ddbbe2970e00356f2fce
devel <systemdevcloud@live.co.uk>
2016-08-22 23:35:07 +0100

    first upload

---
+++ js/script.js
@@ -0,0 +1,36 @@
+
+function loadData() {
+
+    var $body = $('body');
+    var $wikiElem = $('#wikipedia-links');
+    var $nytHeaderElem = $('#nytimes-header');
+    var $nytElem = $('#nytimes-articles');
+    var $greeting = $('#greeting');
+
+    // clear out old data before new request
+    $wikiElem.text("");
+    $nytElem.text("");
+
+    // load streetview
+
+    // YOUR CODE GOES HERE!
+
+
+//var street = $('#street').val();
+ var street= $("input:text[id$='street']").val();;
+  var city= $("input:text[id$='city']").val();
+alert(street);
+alert(city);
+
+$body.append('<img class="bgimg" src="http://example.com/someimage.png">');
+
+
+
+
+    return true;
+};
+
+$('#form-container').submit(loadData);
+
+
+console.log("test info");
